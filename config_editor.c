a86b4ec392306d646de2646e29dec97997ebac3a
LIU YUXIN <liuyuxin070805@163.com>
2026-02-20 18:40:05 +0800

    - 新建了一个简单的代码练手项目,即修改json文件特定值,用于加深对于cJSON处理逻辑的理解

---
+++ config_editor.c
@@ -0,0 +1,11 @@
+#include "cJSON.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(void) {
+    // TODO: 读取文件
+    // TODO: 解析JSON
+    // TODO: 查找并修改 port
+    // TODO: 保存回文件
+    return 0;
+}
